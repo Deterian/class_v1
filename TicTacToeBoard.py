d64595021ca1ff8838c6c53408875548261f0dce
Deterian <deterian.viss@yandex.ru>
2020-11-12 14:39:05 +0300

    12345

--- TicTacToeBoard.py
+++ TicTacToeBoard.py
@@ -81,17 +81,3 @@ class TicTacToeBoard:
                 return 'Клетка ' + str(row) + ', ' + str(col) + ' уже занята'
         else:
             return 'Игра уже завершена'
-
-
-board = TicTacToeBoard()
-print(*board.get_field(), sep="\n")
-print(board.make_move(1, 1))
-print(*board.get_field(), sep="\n")
-print(board.make_move(1, 1))
-print(board.make_move(1, 2))
-print(*board.get_field(), sep="\n")
-print(board.make_move(2, 1))
-print(board.make_move(2, 2))
-print(board.make_move(3, 1))
-print(board.make_move(2, 2))
-print(*board.get_field(), sep="\n")
